bbd4a76bae16603c4e7640924e103fd036f9c411
Aayush-Koju <iush.cozu12@gmail.com>
2024-10-23 08:43:29 +0545

    default router setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BooksProvider } from "./BooksContext";
 import Books from "./components/Books";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BooksProvider>
-      <Books></Books>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books></Books>}></Route>
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
